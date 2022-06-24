39244ee0fd6bd30a90131eab3be4476fd9624a84
Martin Valdivia <martin.valdivia@sensormedia.com>
2022-06-23 20:23:30 -0500

    Use puppeteer for uploading tasks

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Automation test
 
 Run `npm start` to compile typescript and run **/dist/index.js**
+
+> NOTE: You can switch to usingAPI branch to see an implementation of the same task but using todoist API instead of puppeteer.
--- index.ts
+++ index.ts
@@ -13,36 +13,36 @@ interface ITask {
 
 interface ITasks {
     tasks: Array<ITask>,
-    labels: Array<string>
+}
+
+async function startBrowser() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    setTimeout(async () => {
+        await browser.close();
+    }, 60000 * 4);
+
+    return {browser, page};
 }
 
 /**
  * Get a list of tasks with puppeteer from the random list generator url
  */
-const getTaskList = async () => {
-    const browser = await puppeteer.launch();
+const getTaskList = async ({browser}) => {
     const page = await browser.newPage();
 
     await page.goto(RANDOM_LIST_URL, {
         waitUntil: 'networkidle2'
     });
 
-
-    setTimeout(async () => {
-        await browser.close();
-    }, 60000 * 4);
-
-
     return await page.evaluate( ():ITasks => { 
         const tasks : Array<ITask> = [];        
         const taskDivs = document.querySelectorAll('.card-body') 
-        const set_of_labels = new Set<string>();
 
         for (const div of taskDivs) {
             if(tasks.length > 4) break;
 
             const labels = Array.from(div.querySelectorAll('.tags.badge'),({textContent})=> textContent );
-            labels.forEach(label=>set_of_labels.add(label))
 
             tasks.push({
                 title: div.querySelector('.task-title > div').textContent,
@@ -53,56 +53,83 @@ const getTaskList = async () => {
         }
 
         return {
-            tasks,
-            labels: Array.from(set_of_labels)
+            tasks
         };
     });
 }
 
-/**
- * Add tasks to Todoist in a new project 
- */
-const addTaskToDoist = async ({labels,tasks}:ITasks) => {
-    const todo_api = new TodoistApi(`${process.env.API_TOKEN}`);
-    const labels_map = new Map<string,number>();
-
-    // Add new labels to todoist labels 
-    const existing_labels = await todo_api.getLabels();
-    existing_labels.forEach(({name,id})=>labels_map.set(name,id));
-
-    labels.forEach( async (label)=>{
-        if(!labels_map.has(label)){
-            const {id} = await todo_api.addLabel({ name: label })
-            labels_map.set(label,id)
-        }
-    })
-
-    // Add tasks to todoist in a new project
-    const project_name = `List - ${Date.now()}`;
-    const { id:projectId } = await todo_api.addProject({ name: project_name })
-        
-
-    tasks.forEach(({title,description,labels,time})=>{
-        const labels_ids = labels.map(label=>labels_map.get(label));       
-        todo_api.addTask({
-            content: title,
-            projectId,
-            description: `${description} \n Time to complete: ${time}`,
-            labelIds: labels_ids
-        })
-    })
-
-    return {
-        project: project_name,
-        total: tasks.length
-    }
-}
-
 (async ()=>{
     try {
-        const {labels,tasks} = await getTaskList();
-        const {project,total} = await addTaskToDoist({labels,tasks})
-        console.log(`${total} new tasks added to ${project}.`)        
+        const {browser,page} = await startBrowser();
+        const {tasks} = await getTaskList({browser});
+
+        const selectors = {
+            email: '#labeled-input-1',
+            pass:'#labeled-input-3',
+            log:'//button[contains(., "Log in")]',
+            submitTask:'//button[contains(., "Add task")]',
+            addTask:'.plus_add_button',
+            createTag:'.popper > div > ul > li:last-child',
+            toggleTag: '.popper > div > ul > li',
+            editTitle: '.public-DraftEditor-content',
+            newTagInput: '.popper > div > div > input',
+            addTag: '.task_editor__extra_fields > div:nth-child(2) > button',
+            newTagTitle : '.richtextinput'
+        }
+
+        const url_auth = 'https://todoist.com/auth/login';
+        const {PASSWORD,EMAIL} = process.env;
+        page.setViewport({width: 1024, height: 920});
+
+        await page.goto(url_auth);
+
+        await page.click(selectors.email);
+        await page.keyboard.type(EMAIL);
+
+        await page.click(selectors.pass);
+        await page.keyboard.type(PASSWORD);
+
+        const [loginButton] = await page.$x(selectors.log);
+        
+         await Promise.all([
+            loginButton.click(),
+            page.waitForNavigation({waitUntil:'networkidle2'}),
+        ]);
+
+        const addTaskButton = await page.waitForSelector(selectors.addTask,{visible:true,timeout:0});
+        await addTaskButton.click();
+        await page.keyboard.type(' ')
+
+        for(const task of tasks){
+    
+            await page.keyboard.type(task.title)
+            await page.keyboard.press('Tab')
+            await page.keyboard.type(`${task.description} \n Time to complete: ${task.time}`)
+
+            await page.click(selectors.addTag);
+    
+            for(const tag of task.labels){
+                await page.keyboard.type(tag)            
+                const toggleButton = await page.waitForSelector(selectors.toggleTag)
+                const toggleValue = await toggleButton.evaluate(el => el.textContent);
+                if(toggleValue.trim() === 'Label not found')
+                    await page.click(selectors.createTag)
+                else await toggleButton.click(); 
+    
+                await page.click(selectors.newTagInput);        
+                for(let ix = 0;ix < tag.length; ix++)
+                    await page.keyboard.press('Backspace');        
+            }
+    
+            await page.click(selectors.addTag);
+            const [submitButton] = await page.$x(selectors.submitTask);
+            await submitButton.click();
+            await page.click(selectors.newTagTitle);
+
+        }
+
+    
+        
     } catch (error) {
         console.error(error)
     }
